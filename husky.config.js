fc79585645ed85602af99ecd9b030d9dc2dd2d5d
Federico Osorio <fosorioa@gmail.com>
2020-09-03 18:59:17 -0400

    test

--- husky.config.js
+++ husky.config.js
@@ -3,8 +3,8 @@ const tasks = arr => arr.join(' && ')
 module.exports = {
   'hooks': {
     'commit-msg': tasks([
-      'commitlint -E HUSKY_GIT_PARAMS',
-      'echo $HUSKY_GIT_PARAMS'
+        //'commitlint -E HUSKY_GIT_PARAMS',
+        'echo $HUSKY_GIT_PARAMS',
     ])
   }
 }
