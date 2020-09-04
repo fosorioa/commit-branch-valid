4ce6190c899fb18859f8f4f528aceb547389eb71
Federico Osorio <fosorioa@gmail.com>
2020-09-03 20:13:25 -0400

    testppppppppp

--- commitlint/prepare-commit-msg.js
+++ commitlint/prepare-commit-msg.js
@@ -1,8 +1,25 @@
 #!/usr/bin/env node
-
+const childProcessExec = require('child_process').exec;
+const util = require('util');
 const fs = require('fs');
 
-const message = fs.readFileSync(process.argv[2], 'utf8').trim();
-console.log(message);
+const exec = util.promisify(childProcessExec);
 
-process.exit(1)
+addUsernameToCommitMessage();
+
+async function addUsernameToCommitMessage() {
+    const message = fs.readFileSync(process.argv[2], 'utf8').trim();
+    const user = getCurrentUser();
+    console.log(`#${message}`)
+    process.exit(1)
+}
+
+async function getCurrentUser() {
+
+    const usernamOutput = await exec('git var GIT_COMMITTER_IDENT');
+    if (usernamOutput.stderr) {
+        throw new Error(stderr);
+    }
+    const username = usernamOutput.stdout;
+    return username;;
+}
