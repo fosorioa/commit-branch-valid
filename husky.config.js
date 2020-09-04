88a9df5b54892bf526a8f5e3f82277249be95303
Federico Osorio <fosorioa@gmail.com>
2020-09-03 20:51:59 -0400

    #2342423 fix: description - Federico Osorio <fosorioa@gmail.com> 1599180727 -0400

--- husky.config.js
+++ husky.config.js
@@ -2,6 +2,7 @@ const tasks = arr => arr.join(' && ')
  
 module.exports = {
   'hooks': {
+    'pre-commit': 'echo "hola"',
     'commit-msg': tasks([
         'commitlint -E HUSKY_GIT_PARAMS',
         './commitlint/prepare-commit-msg.js $HUSKY_GIT_PARAMS'
